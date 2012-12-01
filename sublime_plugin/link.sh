f84670fc41ccd263212b0619c71f6af8f58b588f
Lauritz Thamsen <lauritz.thamsen@gmail.com>
2012-12-01 14:58:57 +0100

    link.sh in OS X

--- sublime_plugin/link.sh
+++ sublime_plugin/link.sh
@@ -2,6 +2,8 @@
 
 if [ `uname -s` = "Linux" ]; then
     ln infrared_plugin.py ~/.config/sublime-text-2/Packages/User/infrared_plugin.py
+elif [ `uname -s` = "Darwin" ]; then
+    ln infrared_plugin.py ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/infrared_plugin.py
 else
     echo Don\'t know where to link
 fi
