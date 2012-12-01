6868d552fe2c0bc4e84af45c7098fb13f27d4127
Freenerd <nospam@freenerd.de>
2012-12-01 19:48:22 +0100

    Working version
    Takes queries from /:statement
    Returns 404 { status: 'not-found' } if favicon.ico
    Returns 404 { status: 'not-found' } if no query or query failed
    Returns 200 { status: 'sql', query: 'SELECT ...' } if query was found
    
    Execute from Rails root via ruby PATH/TO/infrared.rb
    Make sure that sinatra is in rails gemset

--- infrared.rb
+++ infrared.rb
@@ -1,61 +1,19 @@
 require 'sinatra'
-require 'json'
 
-class RailsLoader
-  RAILS_ROOT = '~/Documents/HPI/ProgMod/rails-example'
-
-  print "Loading Rails ..."
-  require File.join(RAILS_ROOT, 'config', 'environment')
-  puts " done"
-
-  ActiveRecord::Base.establish_connection
-
-  class AbortActiveRecordQuery < Exception; end
-
-  #
-  # Monkey patch Active Record
-  #
-  ActiveRecord::ConnectionAdapters::Mysql2Adapter.class_eval do
-    # These queries will not be executed
-    QUERY_BLACKLIST = %w{ SELECT UPDATE DELETE INSERT ALTER }
-
-    alias_method :old_execute, :execute
-    def execute(sql, name = nil)
-      if QUERY_BLACKLIST.map { |q| !!(sql.match q) }.any?
-        raise AbortActiveRecordQuery, sql
-      else
-        old_execute(sql, name)
-      end
-    end
-  end
-
-  def execute_statement(statement)
-    begin
-      eval(statement)
-    rescue AbortActiveRecordQuery => e
-      p e.message
-      output = e.message
-    end
-    output
-  end
-
-  private
-
-  def self.load_rails(rails_root)
-  end
-
-  def self.patch_active_record
-    puts "Patching Active Record"
-
-  end
-end
+require_relative './lib/rails_loader.rb'
 
 rails = RailsLoader.new
 
-disable :protection
+configure do
+  disable :protection
+end
 
 get '/:statement' do |statement|
-  if /favicon/.match(statement) == nil
-    rails.execute_statement(statement)
+  halt [ 404 ] if /favicon/.match(statement)
+
+  if sql = rails.get_sql(statement)
+    [ 200, {}, { status: 'sql', query: sql }.to_json ]
+  else
+    [ 404, {}, { status: 'not-found' }.to_json ]
   end
 end
---
+++ lib/rails_loader.rb
@@ -0,0 +1,77 @@
+class RailsLoader
+  RAILS_ROOT = '~/Documents/HPI/ProgMod/rails-example'
+
+  #
+  # Load Rails app
+  #
+  print "Loading Rails ..."
+  require File.join(RAILS_ROOT, 'config', 'environment')
+  puts " done"
+
+  #
+  # Monkey patch Active Record
+  #
+
+  # We need to establish a connection, so the Adapters can be monkey patched
+  ActiveRecord::Base.establish_connection
+
+  class AbortActiveRecordQuery < Exception; end
+
+  ActiveRecord::ConnectionAdapters::Mysql2Adapter.class_eval do
+    # These queries will not be executed
+    QUERY_BLACKLIST = %w{ SELECT UPDATE DELETE INSERT ALTER }
+
+    alias_method :old_execute, :execute
+    def execute(sql, name = nil)
+      if QUERY_BLACKLIST.map { |q| !!(sql.match q) }.any?
+        raise AbortActiveRecordQuery, sql
+      else
+        old_execute(sql, name)
+      end
+    end
+  end
+
+  #
+  # done Monkey Patching
+  #
+
+  def get_sql(statement)
+    if has_model?(statement)
+      execute_model_query(statement)
+    else
+      nil
+    end
+  end
+
+  private
+
+  def execute_model_query(statement)
+    output = nil
+
+    begin
+      eval(statement)
+    rescue AbortActiveRecordQuery => e
+      p e.message
+      output = e.message
+    rescue => e
+      p e.message
+    end
+
+    output
+  end
+
+  def has_model?(statement)
+    model_names.map { |m| !!(statement.match m) }.any?
+  end
+
+  def models
+    @models ||= begin
+      Rails.application.eager_load!
+      ActiveRecord::Base.descendants
+    end
+  end
+
+  def model_names
+    models.map(&:name)
+  end
+end
