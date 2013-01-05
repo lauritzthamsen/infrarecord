ee77bd4854b113233d99928b4751df3451afa838
Fabian Bornhofen <fbornhofen@gmail.com>
2013-01-05 10:40:03 +0100

    started bindings replacement in server

--- rails_gem/app/controllers/infrarecord/infrarecord_controller.rb
+++ rails_gem/app/controllers/infrarecord/infrarecord_controller.rb
@@ -2,8 +2,9 @@ module Infrarecord
   class InfrarecordController < ApplicationController
     def statement
       p "Statement is '#{params[:statement]}'"
+      p "Bindings is '#{params[:bindings]}'"
 
-      orm_call = parser.first_possible_orm_call(params[:statement])
+      orm_call = parser.first_possible_orm_call(params[:statement], params[:bindings])
       if orm_call == nil
         render :status => 404, :text => { status: 'not-found' }.to_json
       else
