22c0eefb779fca31efe1adf6abddef534bf5eba7
Lauritz Thamsen <lauritz.thamsen@gmail.com>
2013-02-21 10:05:44 +0100

    removed earlier spikes

--- eval_server/eval_server.rb
+++
@@ -1,9 +0,0 @@
-require 'sinatra'
-require 'json'
-
-get '/:statement' do |statement|
-  halt [ 404 ] if /favicon/.match(statement)
-
-  [ 200, {}, { status: 'eval', query: nil, result: eval(statement) }.to_json ]
-end
-
--- sublime_plugin/link.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ `uname -s` = "Linux" ]; then
-    ln infrared_plugin.py ~/.config/sublime-text-2/Packages/User/infrared_plugin.py
-elif [ `uname -s` = "Darwin" ]; then
-    ln infrared_plugin.py ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/infrared_plugin.py
-else
-    echo Don\'t know where to link
-fi
